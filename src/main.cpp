814fbed1c3bb4e28534e7904ef178e99c2d5b358
bobmcw <jasiekz2014@gmail.com>
2025-04-05 23:41:09 +0200

    Added rendering the player and added handling player input to the event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include "player.h"
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
+    auto player = Player(window);
 
     while (window.isOpen())
     {
+        window.clear();
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
+            player.HandleMove(event);
         }
-
-        window.clear();
+        player.draw();
         window.display();
     }
 }

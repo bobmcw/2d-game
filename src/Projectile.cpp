be3ffbfd1c85ae10c7fb6e1fb62c064d90070dac
bobmcw <jasiekz2014@gmail.com>
2025-04-21 18:20:43 +0200

    added a bullet texture

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -6,7 +6,7 @@
 Projectile::Projectile(sf::Vector2f velocity, sf::Vector2f initialPosition): sprite(this->texture) {
     this->velocity = velocity;
     this->texture = sf::Texture();
-    if (!this->texture.loadFromFile("Assets/textures/wall.png")) {
+    if (!this->texture.loadFromFile("Assets/textures/bullet.png")) {
         std::cerr << "failed to load texture texture/bullet.png";
     }
     this->texture.setRepeated(true);

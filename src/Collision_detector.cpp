b7d60ed333c3e1d913d21b9e4ecfab216a39bec8
bobmcw <jasiekz2014@gmail.com>
2025-05-16 18:06:52 +0200

    changed the order of collision detection

--- src/Collision_detector.cpp
+++ src/Collision_detector.cpp
@@ -51,22 +51,8 @@ void Collision_detector::checkColisionWithPlayer() {
         }
         if (this->player.getSprite().getGlobalBounds().findIntersection((*t).getSprite().getGlobalBounds()) && (*t).
             hasCollision) {
-            //from bottom
-            if (this->player.getSprite().getPosition().y > (*t).getSprite().getPosition().y) {
-                player.getSprite().setPosition({
-                    this->player.getSprite().getPosition().x,
-                    (*t).getSprite().getPosition().y + (*t).getSprite().getGlobalBounds().size.y
-                });
-            }
-            //from top
-            else if (this->player.getSprite().getPosition().y < (*t).getSprite().getPosition().y) {
-                player.getSprite().setPosition({
-                    this->player.getSprite().getPosition().x,
-                    (*t).getSprite().getPosition().y - (*t).getSprite().getGlobalBounds().size.y
-                });
-            }
             //from right
-            else if (this->player.getSprite().getPosition().x > (*t).getSprite().getPosition().x) {
+            if (this->player.getSprite().getPosition().x > (*t).getSprite().getPosition().x) {
                 player.getSprite().setPosition({
                     (*t).getSprite().getPosition().x + (*t).getSprite().getGlobalBounds().size.x,
                     this->player.getSprite().getPosition().y
@@ -77,6 +63,20 @@ void Collision_detector::checkColisionWithPlayer() {
                 player.getSprite().setPosition({
                     (*t).getSprite().getPosition().x - (*t).getSprite().getGlobalBounds().size.x,
                     this->player.getSprite().getPosition().y
+                });
+            }
+            //from bottom
+            else if (this->player.getSprite().getPosition().y > (*t).getSprite().getPosition().y) {
+                player.getSprite().setPosition({
+                    this->player.getSprite().getPosition().x,
+                    (*t).getSprite().getPosition().y + (*t).getSprite().getGlobalBounds().size.y
+                });
+            }
+            //from top
+            else if (this->player.getSprite().getPosition().y < (*t).getSprite().getPosition().y) {
+                player.getSprite().setPosition({
+                    this->player.getSprite().getPosition().x,
+                    (*t).getSprite().getPosition().y - (*t).getSprite().getGlobalBounds().size.y
                 });
             }
         }

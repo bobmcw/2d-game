5dcbaf9324cf4bc6bd09aaeb20caf17d43599014
bobmcw <jasiekz2014@gmail.com>
2025-05-17 12:04:02 +0200

    cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,39 @@
 #include "UI.h"
 #include "SaverAndLoader.h"
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project",State::Fullscreen);
+int main() {
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project", State::Fullscreen);
     window.setFramerateLimit(144);
 
     auto projectileManager = ProjectileManager(window);
-    auto player = Player(window,projectileManager);
+    auto player = Player(window, projectileManager);
     auto enemyController = EnemyController(window, player);
-    auto ui = UI(window,player);
-    auto maploader = Map_parser(window,enemyController,projectileManager);
-    auto saveManager = SaverAndLoader(player,enemyController,maploader);
-    auto menu = Menu(window,saveManager,maploader,player);
+    auto ui = UI(window, player);
+    auto maploader = Map_parser(window, enemyController, projectileManager);
+    auto saveManager = SaverAndLoader(player, enemyController, maploader);
+    auto menu = Menu(window, saveManager, maploader, player);
 
-    //maploader.load_maps();
-    //saveManager.save();
-    //maploader.load_next_map();
+    auto collision_detector = Collision_detector(player, maploader.get_loaded_sprites(), projectileManager,
+                                                 enemyController, maploader, saveManager);
 
-    auto collision_detector = Collision_detector(player, maploader.get_loaded_sprites(),projectileManager,enemyController,maploader,saveManager);
-
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         window.clear();
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
             player.listenForKeyPresses(event);
             menu.listenToKeyPresses(event);
         }
         if (!menu.isActive()) {
-            //drawing objects
             maploader.draw_current_map();
             projectileManager.updateProjectiles();
             ui.update();
-            //collision
             collision_detector.update();
-            //updating objects
             player.update();
             enemyController.updateEnemies();
-            //check if player died
             menu.checkIfPlayerIsDead();
-        }
-        else {
+        } else {
             menu.displayMenu();
         }
         window.display();
